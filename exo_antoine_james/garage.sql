4f2d81e6aaf4993d330702d4adb643800fffdf68
KayZaaH <97876943+KayZaaH@users.noreply.github.com>
2023-05-04 13:59:19 +0200

    Create garage.sql

---
+++ exo_antoine_james/garage.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 04 mai 2023 à 11:57
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `garage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voiture`
+--
+
+DROP TABLE IF EXISTS `voiture`;
+CREATE TABLE IF NOT EXISTS `voiture` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marques` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `models` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `kilometres` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `voiture`
+--
+
+INSERT INTO `voiture` (`id`, `marques`, `models`, `kilometres`) VALUES
+(1, 'Porsche', '911 Turbo', '50000'),
+(3, 'undefined', 'undefined', 'undefined'),
+(4, 'undefined', 'undefined', 'undefined'),
+(5, 'undefined', 'undefined', 'undefined'),
+(6, 'undefined', 'undefined', 'undefined');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
